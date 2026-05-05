90afa53fff47b9a86df128952ff2d26db48e6dbd
Pierce876 <121518742+Pierce876@users.noreply.github.com>
2026-05-04 20:17:14 -0400

    Add PowerShell script to disable consumer experiences
    This script disables Microsoft consumer experiences by setting a registry value.

---
+++ STIG-ID-WN11-CC-000197.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    This PowerShell script turns off Microsoft consumer experiences.
+
+.NOTES
+    Author          : Pierce Pounall
+    LinkedIn        : https://www.linkedin.com/in/pierce-pounall-0478aa22b/?isSelfProfile=false
+    GitHub          : https://github.com/Pierce876
+    Date Created    : 2026-05-04
+    Last Modified   : 2026-05-04
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000197
+
+.TESTED ON
+    Date(s) Tested  : 5/4/2026
+    Tested By       : Pierce Pounall
+    Systems Tested  : Windows 11 Pro
+    PowerShell Ver. : 
+
+.USAGE
+    Run PowerShell as Administrator.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN11-CC-000197.ps1
+#>
+
+# Define the registry path and value
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent"
+$valueName = "DisableWindowsConsumerFeatures"
+$valueData = 1
+
+# Check if the registry path exists, if not create it
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+
+# Set the DisableWindowsConsumerFeatures value
+New-ItemProperty -Path $registryPath -Name $valueName -Value $valueData -PropertyType DWord -Force | Out-Null
+
+# Output success message
+Write-Host "Registry value '$valueName' set to '$valueData' at '$registryPath'."
